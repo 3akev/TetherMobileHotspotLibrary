272176fd2bde87ba76a0fb62cf8eb74deae954b9
ChemicalInk <38759407+ChemicalInk@users.noreply.github.com>
2018-06-26 21:02:47 +0300

    Add project files.

---
+++ TetherMobileHotspotLibrary/TetherMobileHotspot.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Networking.Connectivity;
+using Windows.Networking.NetworkOperators;
+
+namespace TetherMobileHotspotLibrary
+{
+    public class TetherMobileHotspot
+    {
+        private NetworkOperatorTetheringManager tetheringManager;
+
+        public TetherMobileHotspot()
+        {
+            ConnectionProfile InternetConnectionProfile = NetworkInformation.GetInternetConnectionProfile();
+            tetheringManager = NetworkOperatorTetheringManager.CreateFromConnectionProfile(InternetConnectionProfile);
+        }
+
+        public TetheringOperationStatus StartTethering()
+        {
+            return StartTetheringAsync().GetAwaiter().GetResult();
+        }
+
+        public async Task<TetheringOperationStatus> StartTetheringAsync()
+        {
+
+            NetworkOperatorTetheringOperationResult result = await tetheringManager.StartTetheringAsync();
+            return result.Status;
+        }
+
+        public TetheringOperationalState GetTetheringOperationalState()
+        {
+            return tetheringManager.TetheringOperationalState;
+        }
+
+        public String GetClientCount()
+        {
+            return tetheringManager.ClientCount.ToString() + " / " + tetheringManager.MaxClientCount.ToString();
+        }
+    }
+}
